87a0c6c3a698784e022fdd368bb4b54de1e679c3
Arpitpundir <thakurarpitpundir73@gmail.com>
2023-12-28 12:24:24 +0530

    Creates a producer of values with help of generators

--- Generators/Generator_Iterator.js
+++ Generators/Generator_Iterator.js
@@ -1,18 +1,24 @@
-function *something(){
-    var nextVal;
-    while(true){
-        if(nextVal === undefined){
-            nextVal = 1;
-        }else {
-            nextVal = (3 * nextVal) + 6;
+/**
+ * Returned value of generator is an iterator which is also an iterable
+ * Generator can be used to produce values, such that current value is produced from last value which is in state
+ * Below func something yields a new value everytime next is called
+ * Everytime next is called loop runs again and produces a new value
+ */
+function* something() {
+    var nextVal
+    while (true) {
+        if (nextVal === undefined) {
+            nextVal = 1
+        } else {
+            nextVal = 3 * nextVal + 6
         }
-        yield nextVal;
+        yield nextVal
     }
 }
 
-for(let v of something()){
-    console.log(v);
-    if(v > 500){
-        break;
+for (let v of something()) {
+    console.log(v)
+    if (v > 500) {
+        break
     }
 }
