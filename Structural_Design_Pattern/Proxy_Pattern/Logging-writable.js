ba7dfad9e00716c175828b7785aac39ebe39e96e
Arpitpundir <thakurarpitpundir73@gmail.com>
2024-04-03 23:44:59 +0530

    Using writable proxy

--- Structural_Design_Pattern/Proxy_Pattern/Logging-writable.js
+++ Structural_Design_Pattern/Proxy_Pattern/Logging-writable.js
@@ -1,4 +1,11 @@
-export function createLoggingWritable (writable) {
+import { createWriteStream } from 'fs';
+/**
+ * Goal is to create a proxy object for a stream 
+ * such that whenever we write some data to the stream,
+ * then this data is logged as well
+ */
+
+function createLoggingWritable (writable) {
     return new Proxy(writable, { 
       get (target, propKey, receiver) { 
         if (propKey === 'write') { 
@@ -11,4 +18,16 @@ export function createLoggingWritable (writable) {
         return target[propKey] 
       }
     })
-  }
+}
+
+const writable = createWriteStream('test.txt');
+const writableProxy = createLoggingWritable(writable);
+
+writableProxy.write('First chunk');
+writableProxy.write('Second chunk');
+/*
+ because we are calling write on writable directly without passing this 
+ call through proxy and hence no logging behaviour
+*/
+writable.write('This is not logged');
+writableProxy.end();
